a89f47100fc65f31258ee408eb7dd418dfe9f0ea
lambiengcode <lambiengcode@gmail.com>
2023-07-06 10:15:24 +0700

    update .

---
+++ dockerfile
@@ -0,0 +1,15 @@
+FROM node:20-alpine
+
+# WORKDIR
+WORKDIR /app
+
+# COPY
+COPY . .
+
+# install dependencies & build
+RUN rm -rf node_modules
+RUN yarn
+RUN yarn build
+
+# run image
+CMD [ "yarn", "start:prod" ]
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import databaseConfig from './config/database.config';
 import authConfig from './config/auth.config';
