a5eb759d4f61cac34c510405f3bb738302d4a5cd
lambiengcode <lambiengcode@gmail.com>
2024-04-21 08:59:30 +0700

    fix(pm2): change name without spaces

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,20 +1,19 @@
 module.exports = {
-    apps: [
-      {
-        name: "Waterbus Server API",
-        script: "dist/main.js",
-        instances: "max", // can set is "max" for using max of processors
-        autorestart: true,
-        watch: false,
-        max_memory_restart: "2G",
-        exec_mode: "cluster",
-        env: {
-          NODE_ENV: "production",
-        },
-        error_file: "logs/error.log",
-        out_file: "logs/out.log",
-        log_date_format: "YYYY-MM-DD HH:mm:ss",
+  apps: [
+    {
+      name: 'waterbus.server.api',
+      script: 'dist/main.js',
+      instances: 'max', // can set is "max" for using max of processors
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '2G',
+      exec_mode: 'cluster',
+      env: {
+        NODE_ENV: 'production',
       },
-    ],
-  };
-  
+      error_file: 'logs/error.log',
+      out_file: 'logs/out.log',
+      log_date_format: 'YYYY-MM-DD HH:mm:ss',
+    },
+  ],
+};
