032b39e0bc0a61b1c44132e16d712d02adc87c69
lambiengcode <lambiengcode@gmail.com>
2024-04-22 15:44:50 +0700

    lint .

--- src/core/dtos/chats/send-message.dto.ts
+++ src/core/dtos/chats/send-message.dto.ts
@@ -2,7 +2,7 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsString, IsNotEmpty } from 'class-validator';
 
 export class SendMessageDto {
-  @ApiProperty({type: String})
+  @ApiProperty({ type: String })
   @IsString()
   @IsNotEmpty()
   data;
